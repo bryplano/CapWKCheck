02c81a8072475b8e097ae8e28d1679aa1a250859
Bryant Plano <bplano@Bryants-MacBook-Pro.local>
2019-09-09 10:18:20 -0500

    Adding plugin stuff

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrowserView } from '@sethlilly/capacitor-browser-view';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  ngOnInit() {
+    console.log('On init, using BrowserView');
+    BrowserView.open({url: 'https://google.com' });
+  }
 
 }
